0de7539495a7f2a34f4f32d38f2542b7cbdea9a6
Ed Brindley <edward.brindley@skybettingandgaming.com>
2016-07-13 11:22:02 +0100

    Tidy code and update readme

--- README.md
+++ README.md
@@ -1,1 +1,73 @@
-cylon.js
+# Very rough demo of AWS IoT/ Johnny Five/ One Wire Bus on the Raspberry Pi
+
+## Overview
+Runs on Raspberry Pi with camera board mounted on pan/tilt, 
+with connected DS18b20 One Wire temperature sensor, button and LED
+
+Can remotely control camera pan/tilt, take photos and upload them to S3
+Publishes temperature and status of button 
+
+http://johnny-five.io/
+
+https://aws.amazon.com/iot/how-it-works/
+
+## Software prerequisites
+* Raspbian
+* Node 4 LTS 
+```
+# Using Debian, as root
+curl -sL https://deb.nodesource.com/setup_4.x | bash -
+apt-get install -y nodejs
+```
+* NPM 3
+
+## Hardware used
+* Raspberry Pi 3
+* Raspberry Pi camera
+* Cyntech GPIO breakout
+* Camera Pan/tilt mount and 2 micro servos
+* Breadboard
+* Jumper wires
+* 1x LED
+* 1x Microswitch
+* 1x DS18b20 One Wire temperature sensor
+* 1x 10K resistor (for one wire bus)
+
+## How to use
+Enable the camera and One Wire in raspberry pi settings and reboot
+
+Connect LED/ buttons/ One Wire temperature sensor to appropriate GPIO pins as per definitions in index.js (See http://www.modmypi.com/blog/ds18b20-one-wire-digital-temperature-sensor-and-the-raspberry-pi for details about connecting one wire temperature sensor and verifying it is working)
+
+Create a "thing" in AWS IoT console, and download credential files to your device
+
+Load kernel modules for one wire bus
+```
+sudo modprobe wire
+sudo modprobe w1-gpio
+sudo modprobe w1-therm
+```
+
+Export your AWS credentials for the image upload
+
+```
+export AWS_ACCESS_KEY_ID='AKID'
+export AWS_SECRET_ACCESS_KEY='SECRET'
+```
+
+Edit AWS S3 bucket name/ AWS IoT details/path to credentials files in index.js
+
+```
+npm install
+node index.js
+```
+
+From the AWA IoT console, you can then subscribe to the topics "button" and "temperature"
+
+Publishing to the topic camera_shutter takes a photo (must send an empty JSON object as payload)
+
+Publishing to the topic camera_aim with e.g body ```{"x" : 55, "y": 180}``` will set the positions of the pan/tilt servos to point the camera
+
+Pressing the button will toggle the LED status and publish message indicating the button status
+
+Files in common and device from AWS IoT SDK: 
+https://github.com/aws/aws-iot-device-sdk-js
--- index.js
+++ index.js
@@ -1,11 +1,3 @@
-/* AWS IoT/ Johnny Five/ One Wire Bus demo project
- * 
- * Load kernel modules for one wire bus
- * sudo modprobe wire
- * sudo modprobe w1-gpio
- * sudo modprobe w1-therm 
- */
-
 var raspi = require('raspi-io');
 var five = require('johnny-five');
 var w1bus = require('node-w1bus');
@@ -23,138 +15,130 @@ const deviceModule = require('./device');
 const certPath = "/home/pi/Downloads/";
 
 const device = deviceModule({
-	 region: 'eu-west-1',
-	 protocol: 'mqtts',
-	 clientId: "raspberry_pi",
-	 keyPath: certPath + 'private.pem.key',
-	 certPath: certPath + 'certificate.pem.crt',
-	 caPath: certPath + 'root-CA.crt',
-	 testMode: 1,
-	 baseReconnectTimeMs: 4000,
-	 keepalive: 30,
-	 delay: 4000,
-	 debug: false
+  region: 'eu-west-1',
+  protocol: 'mqtts',
+  clientId: "raspberry_pi",
+  keyPath: certPath + 'private.pem.key',
+  certPath: certPath + 'certificate.pem.crt',
+  caPath: certPath + 'root-CA.crt',
+  testMode: 1,
+  baseReconnectTimeMs: 4000,
+  keepalive: 30,
+  delay: 4000,
+  debug: false
 });
-
-var timeout;
-var count = 0;
-const minimumDelay = 250;
 
 device.subscribe('camera_aim');
 device.subscribe('camera_shutter');
 
-
-// Johnny five stuff
+// Initialise Johnny Five
 var board = new five.Board({
   io: new raspi()
 });
 
-var button;
-
 var bus = w1bus.create();
-var config = bus.getConfig();
 
+// When board has initialised
 board.on('ready', function() {
+  // define Johnny Five devices and their pin mappings
   var led = new five.Led('GPIO25');
 
-  button = new five.Button({pin: "GPIO17", isPullup: true});
-  
+  var button = new five.Button({pin: "GPIO17", isPullup: true});
+
   var yservo = new five.Servo({pin: "GPIO18",
-	  center: true
-	});
-	
-  var xservo = new five.Servo({ pin: "GPIO19", 
-    center: true 
+    center: true
   });
-  
-    button.on("down", function () {
-	console.log("down");
-	led.toggle();
-	device.publish('button', JSON.stringify({
-		state: "down"
-	 }));
+
+  var xservo = new five.Servo({ pin: "GPIO19",
+    center: true
   });
-  
+
+  button.on("down", function () {
+    console.log("down");
+    led.toggle();
+    device.publish('button', JSON.stringify({
+      state: "down"
+    }));
+  });
+
   button.on("up", function() {
-	console.log("up");
-
-	device.publish('button', JSON.stringify({
-		state: "up"
-	 }));
-	});
-  
-  this.repl.inject({
-	xservo: xservo,
-	yservo: yservo
+    console.log("up");
+    device.publish('button', JSON.stringify({
+      state: "up"
+    }));
   });
 
+  // Makes servo objects available in REPL
+  this.repl.inject({
+    xservo: xservo,
+    yservo: yservo
+  });
+
+  // Publishes the temperature read from all connected One Wire devices to AWS IoT
   function uploadTemperature() {
-  bus.listAllSensors()
-	.then(function(data){
-	data.ids.map(function(id) {
-	bus.getValueFrom(id, "temperature")
-	.then(function(res){
-		device.publish('temperature', JSON.stringify({
-		sensor_id: id,
-		data: res
-	 }));
-	});
-	});
-	});
-	setTimeout(uploadTemperature, 5000);
+    bus.listAllSensors().then(function(data){
+      data.ids.map(function(id) {
+        bus.getValueFrom(id, "temperature").then(function(res) {
+          device.publish('temperature', JSON.stringify({
+            sensor_id: id,
+            data: res
+          }));
+        });
+      });
+    });
+    setTimeout(uploadTemperature, 5000);
   }
   uploadTemperature();
 
-
-  
-camera.on("read", function(err, timestamp, filename){ 
-  if (filename.endsWith("jpg")) {
-	console.log(filename);
-	var body = fs.createReadStream(filename);
-	var s3obj = new AWS.S3({params: {Bucket: 'iot.maidavale.org', Key: timestamp + ".jpg"}});
-	s3obj.upload({Body: body}).
-	  on('httpUploadProgress', function(evt) { console.log(evt); }).
-	  send(function(err, data) { console.log(err, data) });
-  }
-});
-  
-device.on('message', function(topic, payload) {
-  const message = JSON.parse(payload);
-	switch(topic) {
-		case "camera_aim": {
-			console.log(message.x);
-			console.log(typeof(message.x));
-			console.log(message.y);
-			xservo.to(message.x, 500);
-			yservo.to(message.y, 500);
-			break;
-		  }
-		  case "camera_shutter": {
-			camera.start(); 
-		  }
-		  
-	  }
-	 console.log('message', topic, payload.toString());
+  // Fires when the camera saves file, uploads image to S3
+  camera.on("read", function(err, timestamp, filename){
+    if (filename.endsWith("jpg")) {
+      console.log(filename);
+      var body = fs.createReadStream(filename);
+      var s3obj = new AWS.S3({params: {Bucket: 'iot.maidavale.org', Key: timestamp + ".jpg"}});
+      s3obj.upload({Body: body}).
+        on('httpUploadProgress', function(evt) { console.log(evt); }).
+        send(function(err, data) { console.log(err, data) });
+    }
   });
 
+  // When message received on subscribed topic from AWS IoT
+  device.on('message', function(topic, payload) {
+    const message = JSON.parse(payload);
+    switch(topic) {
+      case "camera_aim": {
+        console.log(message.x);
+        console.log(typeof(message.x));
+        console.log(message.y);
+        xservo.to(message.x, 500);
+        yservo.to(message.y, 500);
+        break;
+      }
+      case "camera_shutter": {
+        camera.start();
+      }
+    }
+    console.log('message', topic, payload.toString());
+  });
 });
 
+// Logging for AWS IoT events
 device.on('connect', function() {
-	 console.log('connect');
+  console.log('connect');
 });
-  
+
 device.on('close', function() {
-	 console.log('close');
+  console.log('close');
 });
-  
+
 device.on('reconnect', function() {
-	 console.log('reconnect');
+  console.log('reconnect');
 });
-  
+
 device.on('offline', function() {
-	 console.log('offline');
+  console.log('offline');
 });
-  
+
 device.on('error', function(error) {
-	 console.log('error', error);
+  console.log('error', error);
 });
